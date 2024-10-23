6ae98c90e74d1557b345ba45f354076aeec7be2a
franchsli <112600427+franchsli@users.noreply.github.com>
2024-10-22 21:42:59 -0500

    href actualizados

--- app.js
+++ app.js
@@ -1,45 +1,49 @@
-function load(html_url, target) {
-   target.innerHTML = ''
-   // peticion del archivo html
-   fetch(html_url)
-   // retornamos la respuesta en forma de texto
-    .then((respuesta) => {
-        return
-    })
-    // cogemos ese texto y lo ponemos
-    // dentro del objetivo deseado (target)
-    .then((html) => {
-        target.innerHTML = html
-    })
-}
-
+/**
+ * 
+ * @param {String} classname 
+ * @param {HTMLElement} element 
+ */
 function apply_unique_class(classname, element){
-    const elemento_con_la_clase = document.querySelector(`.${classname}`)
-    elemento_con_la_clase.classList.remove(classname)
+    const element_already_with_class = document.querySelector(`.${classname}`)
+    element_already_with_class.classList.remove(classname)
     element.classList.add(classname)
 }
 
+/**
+ * 
+ * @param {String} html_file_url 
+ * @param {HTMLElement} target 
+ */
+function load_unique_html(html_file_url, target){
+    target.innerHTML = ''
+    fetch(html_file_url)
+    .then((response) => {
+        return response.text()
+    })
+    .then((html) => {
+        target.innerHTML = html})
+
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-    // seleccionamos todos los enlaces
-    const enlaces = document.getElementsByTagName('a')
-    // agregamos una clase al primer enlace (enlace de la pagina de inicio)
-    enlaces[0].classList.add('active')
-    // seleccionamos el elemento que va a contener los archivos cargados (main)
+    const links = document.getElementsByTagName('a')
+    links[0].classList.add('active')
     const main = document.querySelector('main')
-    // iteramos sobre los enlaces
-    for (let index = 0; index < enlaces.length; index++) {
-        const enlace = enlaces[index];
-        enlace.onclick = (event) => {
-            // si el usuario cliquea
-            // el enlace de inicio
-            if (enlace.textContent === 'inicio') {
+    for (let index = 0; index < links.length; index++) {
+        const link = links[index];
+        link.onclick = (event) => {
+            if(link.textContent !== 'Inicio'){
+                event.preventDefault()
+                //event.stopPropagation()
+                console.log(link.href)
+                load_unique_html(link.href, main)
+                apply_unique_class('active', link)
+            }
+            else{ 
+                // reloads the webpage
                 location.reload()
             }
-            else{
-                event.preventDefault()
-                load(enlace.href , main)
-            }
+
         }
     }
 })
